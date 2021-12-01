e0c05cf650cfb938d4171d1f2c364c307f8b8319
cristianadondero <91587462+cristianadondero@users.noreply.github.com>
2021-12-01 17:49:11 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+#include "lib.h"
+bool numeriprimi (int n, int d){
+    if(d==0 || d==1){
+        return true;
+    }else{
+        int c=n%d;
+        if(c!=0){
+            numeriprimi(n, d-1);
+        }else{
+            return false;
+        }
+    }
 
+
+}
