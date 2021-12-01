e6672946344da2ac012f7ef1d9fe8f02f69e041a
cristianadondero <91587462+cristianadondero@users.noreply.github.com>
2021-12-01 22:35:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main(){
     int a=0, b=2;
     cin>>a;
-    bool ris=numeriprimi(a,b);
+    bool ris= numeriprimi (a,b);
     if(ris){
-        cout<<"numero primo";
+        cout<<"numero primo"<<endl;
     }else{
-        cout<<"numero non primo";
+        cout<<"numero non primo"<<endl;
     }
     return 0;
 
