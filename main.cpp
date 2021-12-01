0887d9449e1792699a57e265d316668f78959389
cristianadondero <91587462+cristianadondero@users.noreply.github.com>
2021-12-01 17:51:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
-  return 0;
+    int n;
+    cin>>n;
+    int ris=numeriprimi(n,n-1);
+    if(ris){
+        cout<<"numeroprimo";
+    }
+    else{
+        cout<<"numero non primo";
+    }
+    return 0;
+
 }
